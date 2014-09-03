69d7e049d011980d0fb09e4f36b64326f86f732e
Alex Wood <awood@redhat.com>
2014-09-03 00:55:30 -0400

    Binding to the root logger interferes with Flask's logging somehow.

--- manage.py
+++ manage.py
@@ -2,31 +2,25 @@
 
 from __future__ import print_function, division, absolute_import
 
+from logging import StreamHandler, Formatter, getLogger
 import logging
 
-from flask import Config
 from flask.ext.script import Manager, Server, Shell
 from flask.ext.assets import ManageAssets
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-    def __init__(self):
-        pass
 
 
 if __name__ == "__main__":
     # Do development app configuration here
     from orbach import app
-    app.config.from_object(DevelopmentConfig())
 
-    root = logging.getLogger()
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    ch.setLevel(logging.DEBUG)
-    root.addHandler(ch)
+    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stderr = StreamHandler()
+    stderr.setFormatter(formatter)
+    stderr.setLevel(logging.DEBUG)
+
+    loggers = [app.logger, getLogger('scss'), getLogger('webassets')]
+    for l in loggers:
+        l.addHandler(stderr)
 
     manager = Manager(app, with_default_commands=False)
     manager.add_command("run", Server(port="8081"))
