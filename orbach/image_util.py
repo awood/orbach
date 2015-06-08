3b9edb6c8cc02f348890759665922e991d5d9807
Alex Wood <awood@redhat.com>
2015-06-08 00:17:14 -0400

    Fix orientation on thumbnails for JPGs.

--- orbach/image_util.py
+++ orbach/image_util.py
@@ -1,6 +1,16 @@
 from pathlib import Path
 
-from PIL import Image
+from PIL import Image, ExifTags
+
+from flask.ext.babel import _
+
+# The Exif codes dictionary has some keys that appear more than once,
+# so we can't just reverse the dictionary
+codes, names = zip(*ExifTags.TAGS.items())
+REVERSE_TAGS = dict(zip(names, [[] for x in names]))
+for c in codes:
+    name = ExifTags.TAGS[c]
+    REVERSE_TAGS[name].append(c)
 
 
 class ImageUtil(object):
@@ -18,8 +28,30 @@ class ImageUtil(object):
 
         img = Image.open(str(image_path))
 
-        # TODO need to fix orientation on thumbnail
-
-        img.thumbnail(self.thumbnail_size)
+        img.thumbnail(self.thumbnail_size, Image.ANTIALIAS)
+        img = self._orient(img)
         img.save(str(destination_path))
         return destination_path
+
+    def _orient(self, img):
+        """Much of this courtesy http://stackoverflow.com/questions/4228530"""
+        if 'exif' not in img.info:
+            # Not a JPG
+            return img
+
+        tags = img._getexif()
+
+        # Orientation should only have one Exif code
+        orientation = tags.get(REVERSE_TAGS['Orientation'][0], None)
+
+        transposition = None
+        if orientation == 3:
+            transposition = Image.ROTATE_180
+        elif orientation == 6:
+            transposition = Image.ROTATE_270
+        elif orientation == 8:
+            transposition = Image.ROTATE_90
+
+        if transposition:
+            img = img.transpose(transposition)
+        return img
---
+++ test/test_image_util.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
+
+from PIL import Image
+
+from orbach.image_util import ImageUtil
+
+
+class ImageUtilTest(unittest.TestCase):
+    def setUp(self):
+        self.resources = os.path.join(os.path.dirname(__file__), 'resources')
+
+    def test_thumbnail_jpg(self):
+        img_path = Path(os.path.join(self.resources, "dog.jpg"))
+        with TemporaryDirectory(prefix=self._testMethodName) as d:
+            test_path = Path(d)
+            thumbnail = ImageUtil().create_thumbnail(img_path, test_path)
+            img = Image.open(str(thumbnail))
+            self.assertEqual((140, 250), img.size)
+
+    def test_thumbnail_png(self):
+        img_path = Path(os.path.join(self.resources, "cat.png"))
+        with TemporaryDirectory(prefix=self._testMethodName) as d:
+            test_path = Path(d)
+            thumbnail = ImageUtil().create_thumbnail(img_path, test_path)
+            img = Image.open(str(thumbnail))
+            self.assertEqual((140, 250), img.size)
+
+    def test_orientation_jpg(self):
+        img = Image.open(os.path.join(self.resources, "dog.jpg"))
+        original_size = list(img.size)
+        reoriented = ImageUtil()._orient(img)
+        self.assertEqual(tuple(reversed(original_size)), reoriented.size)
+
+    def test_orientation_png(self):
+        img = Image.open(os.path.join(self.resources, "cat.png"))
+        with patch.object(img, 'info') as mock_info:
+            mock_info.__contains__.return_value = False
+            ImageUtil()._orient(img)
+            mock_info.__contains__.assert_called_once_with('exif')
