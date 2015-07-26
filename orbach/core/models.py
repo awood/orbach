81459b611a3a98fd2d15e61611693e0edac4858d
Alex Wood <awood@redhat.com>
2015-07-26 17:19:59 -0400

    Update object relationships.

--- orbach/core/models.py
+++ orbach/core/models.py
@@ -16,15 +16,22 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Orbach.  If not, see <http://www.gnu.org/licenses/>.
 '''
+import logging
+
 from django.db import models
 from django.contrib.auth.models import User
 
 from orbach.core.storage import HashDistributedStorage
 
+log = logging.getLogger(__name__)
+
 
 class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "<%s: %s>" % (__class__.__name__, vars(self))
 
     class Meta:
         abstract = True
@@ -34,9 +41,12 @@ class ImageFile(BaseModel):
     hashed_storage = HashDistributedStorage()
     file = models.ImageField(max_length=150, storage=hashed_storage,
          height_field="height", width_field="width")
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, related_name='image_files')
     width = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.file.name
 
     class Meta:
         db_table = "ob_image_files"
@@ -47,7 +57,7 @@ class ImageFile(BaseModel):
 
 class Gallery(BaseModel):
     name = models.CharField(max_length=175)
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, related_name='galleries')
     description = models.TextField(max_length=300)
     parent = models.ForeignKey('self')
 
