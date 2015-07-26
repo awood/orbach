3769446fcf592ea44a56918fda419fb181860766
Alex Wood <awood@redhat.com>
2015-07-26 17:20:14 -0400

    Remove unused import.

--- test/functional/__init__.py
+++ test/functional/__init__.py
@@ -16,8 +16,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Orbach.  If not, see <http://www.gnu.org/licenses/>.
 '''
-import unittest
-
 from selenium import webdriver
 
 from django.core.urlresolvers import reverse
